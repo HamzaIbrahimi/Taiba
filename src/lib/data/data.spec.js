e3ea62333ae4d5482091d99adabb6dce6b48fff2
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2026-02-28 15:00:29 +0100

    Add general tests for the json data

---
+++ src/lib/data/data.spec.js
@@ -0,0 +1,22 @@
+import { describe, it, expect } from 'vitest';
+import times from './times.json';
+
+describe('General object tests', () => {
+	it('Object should not be empty', () => {
+		expect(times).toBeDefined();
+		expect(typeof times).toBe('object');
+		expect(Object.keys(times).length).toBe(366);
+		expect(Object.keys(times)[0]).toMatch(/^\d{1,2}-\d{1,2}$/);
+		expect(Object.keys(times['22-5']).length).toBe(5);
+	});
+});
+
+describe('Check the prayer times for a given date', () => {
+	it('Expects prayer times to match', () => {
+		expect(times['28-2']['fajr']).toBe('05:04');
+		expect(times['28-2']['dhuhr']).toBe('12:24');
+		expect(times['28-2']['asr']).toBe('15:03');
+		expect(times['28-2']['maghrib']).toBe('17:43');
+		expect(times['28-2']['isha']).toBe('19:43');
+	});
+});

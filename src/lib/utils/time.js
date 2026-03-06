111adc79b04c8b6250dc6d76e180a2f99dcd60ce
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2026-03-06 22:17:30 +0100

    Create a function that returns the prayer times Tested and respects DST in CPH

--- src/lib/utils/time.js
+++ src/lib/utils/time.js
@@ -4,14 +4,24 @@ We create a helper functions that will ease this process
 The csv files gives us the time in hours:minutes hence we can increment the hour by one
 if we are in DST
 */
+import times from '$lib/data/times.json';
 
 const pad = n => (n < 10 ? '0' + n : n);
 
-export default function addOneHour(time, date = new Date()) {
+export function addOneHour(time, date = new Date()) {
 	if (date.getTimezoneOffset() === -120) {
 		const splitTime = time.split(':');
 		const incrementedHour = (Number(splitTime[0]) + 1) % 24;
 		return `${pad(incrementedHour)}:${splitTime[1]}`;
 	}
 	return time;
+}
+
+export default function todaysPrayerTimes(today = new Date()) {
+	let [day, month] = [today.getDate(), today.getMonth() + 1];
+	let prayers = times[`${day}-${month}`];
+	for (const key in prayers) {
+		prayers[key] = addOneHour(prayers[key], today);
+	}
+	return prayers;
 }
--- src/lib/utils/time.spec.js
+++ src/lib/utils/time.spec.js
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import times from '$lib/data/times.json';
-import addOneHour from './time';
+import { addOneHour, todaysPrayerTimes } from './time';
 
 describe('General time tests', () => {
 	it('adds one hour if we are in day light saving time otherwise returns normal time', () => {
@@ -10,5 +10,14 @@ describe('General time tests', () => {
 		expect(addOneHour(dayDst, new Date('2026-03-30'))).toBe('21:14');
 		expect(addOneHour(dayDst, new Date(2026, 2, 29, 3))).toBe('21:14');
 		expect(addOneHour(dayDst, new Date(2026, 2, 29, 1, 59, 59))).toBe('20:14');
+	});
+
+	it("provides an object with a given date's prayer times with respect to DST", () => {
+		let prayersNotDst = todaysPrayerTimes(new Date('2026-03-06'));
+		let prayersDst = todaysPrayerTimes(new Date('2026-03-30'));
+		expect(prayersNotDst['fajr']).toBe('04:50');
+		expect(prayersNotDst['dhuhr']).toBe('12:22');
+		expect(prayersDst['fajr']).toBe('04:47');
+		expect(prayersDst['dhuhr']).toBe('13:16');
 	});
 });

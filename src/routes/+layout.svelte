930af599787ee5f0269cc06d0048fddc90448925
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2026-03-06 02:53:26 +0100

    Import consistent components to layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import favicon from '$lib/assets/favicon.svg';
 	import '../reset.css';
 	import '../app.css';
+	import Header from '$lib/components/Header.svelte';
 	let { children } = $props();
 </script>
 
@@ -9,4 +10,5 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<Header />
 {@render children()}

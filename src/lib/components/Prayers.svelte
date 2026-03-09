95c854bf43aca24e422f78cbfb6f8a31078697a2
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2026-03-09 19:49:00 +0100

    Avoid repeition by making the prayer stamp a component

---
+++ src/lib/components/Prayers.svelte
@@ -0,0 +1,47 @@
+<script>
+	import todaysPrayerTimes from '$lib/utils/time';
+	let prayerNames = ['fajr', 'dhuhr', 'asr', 'maghrib', 'isha'];
+	let prayerTimes = todaysPrayerTimes();
+</script>
+
+<dl class="salah-times">
+	{#each prayerNames as name}
+		<div class="prayer">
+			<dt>{name}</dt>
+			<dd>{prayerTimes[name]}</dd>
+		</div>
+	{/each}
+</dl>
+
+<style>
+	.salah-times {
+		display: grid;
+		grid-template-columns: var(--gtc, repeat(5, 1fr));
+		font-family: var(--container--ff, var(--ff-heading));
+		background-color: var(--container-bg);
+		padding-block: var(--container-padding-block, 0);
+		padding-inline: var(--container-padding-in, 0);
+	}
+
+	dt {
+		letter-spacing: 0.2rem;
+		color: var(--dt-color);
+	}
+
+	dd {
+		font-weight: 1000;
+		font-size: 1.3rem;
+		color: var(--dd-color);
+	}
+
+	.prayer {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding-inline: var(--padding-in, 0);
+	}
+
+	.prayer + .prayer {
+		border-left: var(--border-l, none);
+	}
+</style>

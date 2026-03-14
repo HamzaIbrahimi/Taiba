068c7d5aef77321d741cc9ce509584a89afa3572
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2026-03-14 04:28:40 +0100

    Add a reactive calendar component

---
+++ src/lib/components/Calendar.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { prayerTimesInAMonth } from '$lib/utils/time';
+	let prayerNames = ['fajr', 'dhuhr', 'asr', 'maghrib', 'isha'];
+	let today = new Date();
+	const months = [
+		'jan',
+		'feb',
+		'mar',
+		'apr',
+		'maj',
+		'jun',
+		'jul',
+		'aug',
+		'sep',
+		'okt',
+		'nov',
+		'dec'
+	];
+	let currMonth = $state(today.getMonth() + 1);
+	let monthlyPrayers = $derived(prayerTimesInAMonth(currMonth));
+	let jummahTime = new Date().getTimezoneOffset() === -120 ? '13:45' : '12:45';
+</script>

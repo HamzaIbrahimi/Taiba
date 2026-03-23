9ea0536b0c9e5de64768e97324e7701669329810
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2026-03-24 00:01:52 +0100

    Use Hadith API with translation to provide Hadiths translated to Danish

--- scripts/fetch_rand_hadith.py
+++ scripts/fetch_rand_hadith.py
@@ -2,32 +2,39 @@ import requests
 import json 
 from random import randint, choice
 
+'''
+The API used for this website is provided for free by (HadeethEnc.com) API
+HadeethEnc is a project that aims to provide simplified explanations and clear translation 
+of the authentic Prophetic (Prophet Muhammad ﷺ) hadiths.
+---------------------------------------------------------------------------
+If you will use the API make sure you adhere to the following conditions:
+
+"Contents of the project can be used, with the following terms and conditions:
+1. No modification, addition, or deletion of the content.
+2. Clearly referring to the publisher and the source (HadeethEnc.com)."
+'''
+
 def get_random_hadith ():
-    books = {
-        "bukhari": 7564,
-        "muslim": 3023,
-        "abudawud": 3998,
-        "ibnmajah": 4342,
-        "tirmidhi": 3956,
-    }
-    book = choice(list(books.keys()))
-    number = randint(1, books[book])
+  
     try:    
-        res = requests.get(f'https://hadithapi.pages.dev/api/{book}/{number}')
-        res.raise_for_status()
-        data = res.json()
-        return {
-            "hadith": data["hadith_english"].strip(),
-            "narrator": data["header"].strip(),
-            "refno": data["refno"].strip()
-        }
+        categories_url = "https://hadeethenc.com/api/v1/categories/list/?language=da"
+        available_danish_categories = list(map(lambda x: x['id'],requests.get(categories_url).json()))
+        random_category = choice(available_danish_categories)
+        get_random_category = requests.get(f'https://hadeethenc.com/api/v1/hadeeths/list/?language=da&category_id={random_category}').json()
+        hadith_ids_from_category = list(map(lambda x: x['id'], get_random_category["data"]))
+        random_hadith_id = choice(hadith_ids_from_category)
+        get_random_hadith = requests.get(f"https://hadeethenc.com/api/v1/hadeeths/one/?language=da&id={random_hadith_id}").json()
+        return get_random_hadith
+        
+        
     except requests.exceptions.RequestException as e:
         print(f"API call failed: {e}, keeping existing hadith.json")
 
-    
-with open("../src/lib/data/hadith.json", "w") as f:
+
+with open("src/lib/data/hadith.json", "w") as f:
     hadith = get_random_hadith()
     if hadith:
         json.dump(hadith, f, ensure_ascii=False, indent=4)
+        print('****** JSON file successfully created in src/lib/data/hadith.json ******')
     else:
         print("Skipping file write, keeping existing hadith.json")

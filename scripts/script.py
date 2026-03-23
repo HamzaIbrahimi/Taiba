080e724bb3f6f2ede95d190fa022337907d4ce13
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2026-03-24 00:02:27 +0100

    Add script call to package.json

--- scripts/script.py
+++ scripts/script.py
@@ -17,6 +17,6 @@ with open ('times.csv', 'r') as file:
         }
 
 
-with open ('../src/lib/data/times.json', "w", encoding='utf-8') as file:
+with open ('src/lib/data/times.json', "w", encoding='utf-8') as file:
 # Turns the dict into a json file and outputs it to file
     json.dump(prayer_times, file, ensure_ascii=False, indent=4)

29c361392bef343d97d4810d073d2b8ea74a8cbe
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2026-03-30 13:17:26 +0200

    Create footer component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,31 @@
+<script>
+	let today = new Date();
+</script>
+
+<footer class="footer-strip">
+	<div class="footer-logo">TAIBA MOSKÉ</div>
+	<div class="footer-copy">
+		&copy {today.getFullYear()} Taiba Moské · Alle rettigheder forbeholdes
+	</div>
+</footer>
+
+<style>
+	footer {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem;
+		background-color: var(--clr-bg-nav);
+	}
+
+	.footer-logo {
+		color: var(--clr-gold);
+		font-size: 1.2rem;
+		font-family: var(--ff-nav-links);
+	}
+
+	.footer-copy {
+		font-size: 0.7rem;
+		color: var(--clr-sage);
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import '../reset.css';
 	import '../app.css';
 	import Header from '$lib/components/Header.svelte';
+	import Footer from '$lib/components/Footer.svelte';
 	let { children } = $props();
 </script>
 
@@ -12,3 +13,4 @@
 
 <Header />
 <main>{@render children()}</main>
+<Footer />

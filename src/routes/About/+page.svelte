c4207f159b7becf3992325829a3c2fe7848adf46
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2026-04-03 13:12:15 +0200

    Add more content to about page and update packages

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -1,11 +1,12 @@
 <script>
 	import diamond from '$lib/assets/diamond.svg';
+	import gather from '$lib/assets/masjid-photos/event-3.webp';
 </script>
 
 <div class="main-content">
 	<div class="about-us">
 		<h5>Om Os</h5>
-		<div>Vores Historie & <br /> Mission</div>
+		<div>Vores Historie <br /> & Mission</div>
 		<hr />
 		<p>Taiba Moské — I muslimernes tjeneste siden 1992</p>
 	</div>
@@ -25,26 +26,50 @@
 				>Frederikssundsvej 276 i Brønshøj.</a
 			>
 		</p>
-		<p>
-			Endelig har vi fået vores egen bygning, som skal danne rammen om masser af
-			gode gerninger, Tilbedelse og fællesskab. Vi har stadig et stort stykke
-			arbejde foran os, og vi får brug for al den hjælp, vi kan få, for at skabe
-			en levende og succesfuld moské.
-		</p>
 	</div>
+	<div class="stats">
+		<dl>
+			<div>
+				<dt>Grundlagt</dt>
+				<dd>1992</dd>
+			</div>
+			<div>
+				<dt>Medlemmer</dt>
+				<dd>300+</dd>
+			</div>
+			<div>
+				<dt>Programmer</dt>
+				<dd>5</dd>
+			</div>
+			<div class="md-view">
+				<dt>Adresse</dt>
+				<dd>Frederikssundsvej 276 Brønshøj</dd>
+			</div>
+		</dl>
+	</div>
+	<div class="img-container"><img src={gather} alt="" /></div>
 </div>
 
 <style>
-	.main-content > div {
-		padding: 2rem;
-	}
-
 	.main-content {
 		display: grid;
 	}
 
+	.img-container {
+		grid-row: 3/4;
+	}
+
+	.img-container img {
+		max-height: 500px;
+		width: 100%;
+		object-fit: cover;
+		border-radius: 95% 5% 82% 18% / 2% 94% 6% 98%;
+		filter: grayscale(50%);
+	}
+
 	.about-us {
 		background: var(--clr-bg-hero);
+		padding: 2rem;
 	}
 
 	.about-us div {
@@ -57,6 +82,10 @@
 		margin-top: 1rem;
 		color: var(--clr-parchment);
 		font-style: italic;
+	}
+
+	.about-us-info {
+		padding: 2rem;
 	}
 
 	.small-title {
@@ -88,14 +117,114 @@
 		text-transform: uppercase;
 	}
 
-	p + p {
-		margin-top: 1rem;
-	}
-
 	hr {
 		width: 20%;
 		height: 1px;
 		background-color: var(--clr-gold);
 		border: none;
+	}
+
+	.stats {
+		background-color: var(--clr-bg-nav);
+		padding-block: 1rem;
+	}
+
+	dl {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+	}
+
+	dl div {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	dl div + div {
+		border-left: 1px solid rgba(201, 168, 76, 0.7);
+	}
+
+	dl .md-view {
+		display: none;
+	}
+
+	dd {
+		color: var(--clr-gold);
+		font-family: var(--ff-heading);
+		font-size: 2rem;
+	}
+
+	dt {
+		order: 1;
+		font-family: var(--ff-nav-links);
+		color: var(--clr-sage);
+	}
+
+	@media (min-width: 768px) {
+		.main-content {
+			grid-template-columns: 1fr 1fr;
+		}
+
+		.img-container {
+			grid-row: 2/3;
+		}
+
+		.about-us {
+			grid-column: 1/2;
+		}
+
+		.stats {
+			grid-column: 2/3;
+			grid-row: 1/2;
+			background-color: #121212;
+			padding: 2rem;
+		}
+
+		dl {
+			grid-template-columns: 1fr;
+			grid-template-rows: repeat(4, 1fr);
+			height: 100%;
+		}
+
+		dl .md-view {
+			display: flex;
+		}
+
+		.md-view dd {
+			font-size: 1.2rem;
+			color: var(--fc-primary);
+		}
+
+		dl div + div {
+			border-left: none;
+		}
+
+		dl div:not(.md-view) {
+			border-bottom: 1px solid rgba(255, 255, 255, 0.08);
+		}
+
+		dl div {
+			flex-direction: row;
+			justify-content: space-between;
+		}
+
+		dt {
+			order: 0;
+		}
+	}
+
+	@media (min-width: 1200px) {
+		.main-content {
+			grid-template-columns: 1.3fr 1fr;
+		}
+
+		br {
+			display: none;
+		}
+
+		.stats {
+			background: var(--clr-bg-hero);
+			border-left: 1px solid var(--clr-gold);
+		}
 	}
 </style>

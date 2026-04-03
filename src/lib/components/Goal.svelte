2f3b3c05554a27f8dbdbd279aa54d29e2812576e
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2026-04-03 21:03:31 +0200

    Goal component likely to be repeated across

---
+++ src/lib/components/Goal.svelte
@@ -0,0 +1,15 @@
+<script>
+	import getGoal from '$lib/utils/goal';
+	let promise = $state(getGoal());
+</script>
+
+<section class="goal">
+	<h4>Støt Os - Masjid Taiba Indsamling</h4>
+</section>
+
+<style>
+	section {
+		padding: 1rem;
+		background-color: var(--clr-bg-nav);
+	}
+</style>

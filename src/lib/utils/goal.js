1b686872911e37bf2551faa4c514200451d55d1d
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2026-04-03 21:02:53 +0200

    Add async function to fetch goal from google sheet

---
+++ src/lib/utils/goal.js
@@ -0,0 +1,25 @@
+export default async function getGoal() {
+	try {
+		const response = await fetch(
+			'https://docs.google.com/spreadsheets/d/e/2PACX-1vRCNJjFLxdmJVpMOTWdM559y5wWzxZ2ZFlxfmEBhXyx5KlKNLK2Nj0y6H9HXxoWHiQOrIVjcNDDDu1G/pub?output=csv'
+		);
+		const csv = (await response.text()).replace('\r', '');
+		const data = csv.replace('\n', ',').split(',');
+
+		const o = {
+			collected: +data[2],
+			goal: +data[3]
+		};
+
+		if (!o.collected || !o.goal) {
+			throw new TypeError(
+				`The object does not contain numeric values, check the csv file \n
+                you received ${o.collected} and ${o.goal} `
+			);
+		}
+		return o;
+	} catch (e) {
+		console.error(e);
+		throw e;
+	}
+}

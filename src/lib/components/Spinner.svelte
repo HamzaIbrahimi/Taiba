8b2006e996bf2e52302003cdfcad1cbace51e6da
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2026-04-04 15:54:37 +0200

    Add spinner component for promises

---
+++ src/lib/components/Spinner.svelte
@@ -0,0 +1,31 @@
+<div class="circle"></div>
+
+<style>
+	.circle {
+		max-width: 50px;
+		height: 50px;
+		border-radius: 100vw;
+		background-color: inherit;
+		border-left: 1px solid #fff;
+		border-bottom: 1px solid #fff;
+		animation: spin 1s infinite ease-in;
+	}
+
+	@keyframes spin {
+		25% {
+			transform: rotate(90deg);
+		}
+
+		50% {
+			transform: rotate(180deg);
+		}
+
+		75% {
+			transform: rotate(270deg);
+		}
+
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
